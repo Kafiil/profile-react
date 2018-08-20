be4d9b9b403c80ec0dd0f17015485799f4e8ad2e
Kafil <kafil.nasdami@gmail.com>
2018-08-20 02:34:53 +0100

    Implementing a reactstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Navbar } from './components/navbar/navbar.component';
+import { MainNav } from './components/navbar/navbar.component';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <MainNav />
       </div>
     );
   }
