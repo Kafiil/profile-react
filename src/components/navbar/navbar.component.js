dde93e8404916a57b68c89e2abccd557b6be10a4
Kafil Nasdami <kafil.nasdami@gmail.com>
2018-09-21 00:09:00 +0100

    Implement navbar collapse on clicking on an item

--- src/components/navbar/navbar.component.js
+++ src/components/navbar/navbar.component.js
@@ -1,52 +1,59 @@
 import React, { Component } from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    Nav,
-    NavItem,
-    NavLink
-} from 'reactstrap';
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
 import { DataService } from "../../services/data.service";
-import './navbar.css'
+import "./navbar.css";
 
 export class MainNav extends Component {
-    constructor(props) {
-        super(props);
-        const sections = new DataService().getSections()
-            .map((e, i) => {
-                return (
-                    <NavItem key={i}>
-                        <NavLink href={'#' + e.title}>
-                            <i className={'fa ' + e.icon}> </i>&nbsp;
-                            {e.title}</NavLink>
-                    </NavItem>
-                )
-            })
+  constructor(props) {
+    super(props);
+    const sections = new DataService().getSections().map((e, i) => {
+      return (
+        <NavItem key={i} onClick={this.toggleItem}>
+          <NavLink href={"#" + e.title}>
+            <i className={"fa " + e.icon}> </i>
+            &nbsp;
+            {e.title}
+          </NavLink>
+        </NavItem>
+      );
+    });
 
-        this.state = {
-            isOpen: false, sections
-        };
-    }
+    this.state = {
+      isOpen: false,
+      sections
+    };
+  }
 
-    toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
+  toggleItem = () => {
+    if (this.state.isOpen) {
+      this.setState({ isOpen: false });
     }
-    render() {
-        return (
-            <div>
-                <Navbar color="dark" dark expand="md">
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="mx-auto" navbar>
-                            {this.state.sections}
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </div>
-        );
-    }
+  };
 
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="mx-auto" navbar>
+              {this.state.sections}
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
